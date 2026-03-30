174ebca427c2277235b29b2ada9e80c7529076f6
essodong-pro <engnama@byupathway.edu>
2026-03-30 16:12:24 +0000

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
+// Program.cs
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test Assignment
+        Assignment simple = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simple.GetSummary());
+
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
