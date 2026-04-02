e69b04bdeef69ca22bd6ff8bcfed5595e512cec3
essodong-pro <engnama@byupathway.edu>
2026-04-02 17:54:47 +0000

    Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Iterate and display color + area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
